6694cf8fabdb87156315890a664f3240c2a22ca8
gdupeux <36335105+gdupeux@users.noreply.github.com>
2022-04-10 16:59:24 +0200

    Update install.user.js

--- install.user.js
+++ install.user.js
@@ -9,7 +9,7 @@
 var script = document.createElement('script');
 script.id = 'Trimps-o-Matic-script';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://cdn.jsdelivr.net/gh/gdupeux/Trimps-o-Matic@latest/trimps.js';
+script.src = 'https://cdn.jsdelivr.net/gh/LordJimX/Trimps-o-Matic@latest/trimps.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
