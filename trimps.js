63118be5d7208fdc48b695cd810de18cb8ac11ef
gdupeux <36335105+gdupeux@users.noreply.github.com>
2022-04-17 19:37:55 +0200

    Update trimps.js

--- trimps.js
+++ trimps.js
@@ -363,49 +363,49 @@ function debug(text){
     console.log(text);
 }
 
+
+$("head").append("<link href = 'https://code.jquery.com/ui/1.12.1/themes/ui-lightness/jquery-ui.css' rel = 'stylesheet'>");
+$("body").append(
+    $("<div/>", {"id": "ToMsettingsTest", "title": "ToM settings", "style": "font: 12pt Courier New"}).append([
+        $("<table/>").append([
+            $("<tr/>").append([
+                $("<input>", {"type": "checkbox", "id": "storagesCheckTest", "checked": moduleStorage}),
+                $("<label/>", {"for": "storagesCheckTest"}).append(" storage "),
+                $("<br>"),
+                $("<input>", {"type": "checkbox", "id": "structuresCheckTest", "checked": moduleStructure}),
+                $("<label/>", {"for": "structuresCheckTest"}).append(" structures "),
+                $("<br>"),
+                $("<input>", {"type": "checkbox", "id": "jobsCheckTest", "checked": moduleJob}),
+                $("<label/>", {"for": "jobsCheckTest"}).append(" jobs "),
+                $("<br>"),
+                $("<input>", {"type": "checkbox", "id": "equipmentsCheckTest", "checked": moduleEquipment}),
+                $("<label/>", {"for": "equipmentsCheckTest"}).append(" equipments "),
+                $("<br>"),
+                $("<input>", {"type": "checkbox", "id": "upgradesCheckTest", "checked": moduleUpgrade}),
+                $("<label/>", {"for": "upgradesCheckTest"}).append(" upgrades "),
+                $("<br>"),
+                $("<input>", {"type": "checkbox", "id": "mapForEquipmentCheckTest", "checked": moduleMapForEquipment}),
+                $("<label/>", {"for": "mapForEquipmentCheckTest"}).append(" map for equipment "),
+                $("<br>")
+            ]),
+            $("<tr/>").append([
+                $("<input>", {"id": "lumberInput"}),
+                $("<label/>", {"for": "lumberInput"}).append("Lumber"),
+                $("<br>")
+            ]),
+        ])
+    ])
+);
+$(function() {
+    $("#ToMsettingsTest").dialog({
+        autoOpen: false, 
+        buttons: {'Ok': function(event){$("#ToMsettingsTest").dialog("close"); ToMsettingsOpened = false;},
+            'Cancel': function(event){$("#ToMsettingsTest").dialog("close"); ToMsettingsOpened = false;}},
+        width: 300
+    });
+});
+
 function test(){
     debug('Test');
-    $("head").append("<link href = 'https://code.jquery.com/ui/1.12.1/themes/ui-lightness/jquery-ui.css' rel = 'stylesheet'>");
-    $("body").append(
-        $("<div/>", {"id": "ToMsettingsTest", "title": "ToM settings", "style": "font: 12pt Courier New"}).append([
-            $("<table/>").append([
-                $("<tr/>").append([
-                    $("<input>", {"type": "checkbox", "id": "storagesCheckTest", "checked": moduleStorage}),
-                    $("<label/>", {"for": "storagesCheckTest"}).append(" storage "),
-                    $("<br>"),
-                    $("<input>", {"type": "checkbox", "id": "structuresCheckTest", "checked": moduleStructure}),
-                    $("<label/>", {"for": "structuresCheckTest"}).append(" structures "),
-                    $("<br>"),
-                    $("<input>", {"type": "checkbox", "id": "jobsCheckTest", "checked": moduleJob}),
-                    $("<label/>", {"for": "jobsCheckTest"}).append(" jobs "),
-                    $("<br>"),
-                    $("<input>", {"type": "checkbox", "id": "equipmentsCheckTest", "checked": moduleEquipment}),
-                    $("<label/>", {"for": "equipmentsCheckTest"}).append(" equipments "),
-                    $("<br>"),
-                    $("<input>", {"type": "checkbox", "id": "upgradesCheckTest", "checked": moduleUpgrade}),
-                    $("<label/>", {"for": "upgradesCheckTest"}).append(" upgrades "),
-                    $("<br>"),
-                    $("<input>", {"type": "checkbox", "id": "mapForEquipmentCheckTest", "checked": moduleMapForEquipment}),
-                    $("<label/>", {"for": "mapForEquipmentCheckTest"}).append(" map for equipment "),
-                    $("<br>")
-                ]),
-                $("<tr/>").append([
-                    $("<input>", {"id": "lumberInput"}),
-                    $("<label/>", {"for": "lumberInput"}).append("Lumber"),
-                    $("<br>")
-                ]),
-            ])
-        ])
-    );
-    $(function() {
-        $("#ToMsettingsTest").dialog({
-            autoOpen: false, 
-            buttons: {'Ok': function(event){$("#ToMsettingsTest").dialog("close"); ToMsettingsOpened = false;},
-                'Cancel': function(event){$("#ToMsettingsTest").dialog("close"); ToMsettingsOpened = false;}},
-            width: 300
-        });
-    });
-
     $("#ToMsettingsTest").dialog("open");
-
 }
